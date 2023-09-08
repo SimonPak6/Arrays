19a80834c712f49ac0eb9d6fb174d81b3e06d484
Simon Pak <simon.pak@mrjaffesclass.com>
2023-09-08 11:24:02 -0700

    Operators

---
+++ Operators.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class RenameThisClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Operators
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int i;
+        double f = 2.5;
+        boolean flag = true;
+        i = 10;
+        f += i;
+        flag = !flag;
+        i++;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
+    }
+}
